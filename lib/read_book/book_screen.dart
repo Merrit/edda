4e2527d4eedad35a400b191028a3cd9b125d820d
Kristen McWilliam <merritt_public@outlook.com>
2020-09-20 17:35:44 -0400

    Refactor progress

--- lib/read_book/book_screen.dart
+++ lib/read_book/book_screen.dart
@@ -29,7 +29,6 @@ class _BookScreenState extends State<BookScreen> {
     super.initState();
     book = widget.book;
     _pageController = PageController();
-    // _loadBook();
     _getChapters();
   }
 
@@ -51,10 +50,9 @@ class _BookScreenState extends State<BookScreen> {
     });
   }
 
+  /// Page forwards.
   void _goForward() {
-    // if (currentPage != )
-
-    // Add end of book check
+    // TODO: Add end of book check
     if (hasReachedChapterEnd) {
       hasReachedChapterEnd = false;
       hasReachedChapterStart = true;
@@ -70,6 +68,7 @@ class _BookScreenState extends State<BookScreen> {
     }
   }
 
+  /// Page backwards.
   void _goBack() {
     if (hasReachedChapterStart) {
       hasReachedChapterStart = false;
@@ -90,12 +89,9 @@ class _BookScreenState extends State<BookScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: showInterface
-          ? AppBar(
-              title: Text(book.title),
-            )
-          : null,
+      appBar: showInterface ? AppBar(title: Text(book.title)) : null,
       body: FutureBuilder(
+        // Once the chapters are loaded from disk we can display our content.
         future: chapters,
         builder: (BuildContext context, AsyncSnapshot snapshot) {
           if (!snapshot.hasData) {
@@ -103,82 +99,67 @@ class _BookScreenState extends State<BookScreen> {
           } else if (snapshot.hasError) {
             return Center(child: Text('Error loading text'));
           }
+          // Book data has loaded, proceed to display content.
           _checkPosition();
-          // IgnorePointer()
-          return Container(
-            child: Stack(
-              children: [
-                GestureDetector(
-                  onHorizontalDragEnd: (dragEndDetails) {
-                    if (dragEndDetails.primaryVelocity < 0) {
-                      _goForward();
-                    } else if (dragEndDetails.primaryVelocity > 0) {
-                      _goBack();
-                    }
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(2.0),
-                    child: PageView(
-                      controller: _pageController,
-                      children: book.chapters[currentChapter].pages,
-                      physics: NeverScrollableScrollPhysics(),
-                      onPageChanged: (index) {
-                        if (index + 1 ==
-                            book.chapters[currentChapter].pages.length) {
-                          hasReachedChapterEnd = true;
-                        } else if (index == 0) {
-                          hasReachedChapterStart = true;
-                        } else {
-                          hasReachedChapterStart = false;
-                          hasReachedChapterEnd = false;
-                        }
-                      },
-                    ),
-                  ),
-                ),
-                Row(
-                  children: [
-                    // Detect left-side tap
-                    Flexible(
-                      flex: 4,
-                      child: Container(
-                        // color: Colors.lightGreen.withOpacity(0.3),
-                        child: GestureDetector(
-                          behavior: HitTestBehavior.translucent,
-                          onTap: () => _goBack(),
-                        ),
-                      ),
-                    ),
-                    // Detect center tap
-                    Flexible(
-                      flex: 1,
-                      child: Container(
-                        // color: Colors.cyan.withOpacity(0.3),
-                        child: GestureDetector(
-                          behavior: HitTestBehavior.translucent,
-                          onTap: () {
-                            _toggleInterface();
-                          },
-                        ),
-                      ),
-                    ),
-                    // Detect right-side tap
-                    Flexible(
-                      flex: 4,
-                      child: Container(
-                        // color: Colors.red.withOpacity(0.3),
-                        child: GestureDetector(
-                          behavior: HitTestBehavior.translucent,
-                          onTap: () => _goForward(),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          );
+          return bookPageView();
         },
+      ),
+    );
+  }
+
+  Container bookPageView() {
+    return Container(
+      // Stack the GestureDetectors on top of the pages.
+      child: Stack(
+        children: [
+          // GestureDetector works better than the built-in PageView
+          // gestures because we know when a swipe was attempted at the
+          // beginning and / or end of a chapter, so we can switch out to a
+          // new chapter.
+          GestureDetector(
+            onHorizontalDragEnd: (dragEndDetails) {
+              // Use on___DragEnd so we only get one event per swipe.
+              if (dragEndDetails.primaryVelocity < 0) {
+                _goForward();
+              } else if (dragEndDetails.primaryVelocity > 0) {
+                _goBack();
+              }
+            },
+            // Padding so the text doesn't hit right against the screen
+            // edges. In books this would be considered the "margin".
+            // TODO: Make this padding a configurable setting.
+            child: Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: PageView(
+                controller: _pageController,
+                children: book.chapters[currentChapter].pages,
+                // Disable PageView gestures.
+                physics: NeverScrollableScrollPhysics(),
+                onPageChanged: (index) {
+                  if (index + 1 == book.chapters[currentChapter].pages.length) {
+                    hasReachedChapterEnd = true;
+                  } else if (index == 0) {
+                    hasReachedChapterStart = true;
+                  } else {
+                    hasReachedChapterStart = false;
+                    hasReachedChapterEnd = false;
+                  }
+                },
+              ),
+            ),
+          ),
+          // GestureDetectors for Back, Toggle Interface, and Forward taps.
+          Row(
+            children: [
+              // Detect left-side tap
+              TapDetector(flexAmount: 1, tapCallback: _goBack),
+              // Detect center tap
+              TapDetector(flexAmount: 1, tapCallback: _toggleInterface),
+              // Detect right-side tap
+              TapDetector(flexAmount: 1, tapCallback: _goForward),
+            ],
+          ),
+        ],
       ),
     );
   }
@@ -187,5 +168,38 @@ class _BookScreenState extends State<BookScreen> {
   void dispose() {
     _pageController.dispose();
     super.dispose();
+  }
+}
+
+/// A widget that is transparent and contains
+/// a gesture detector and flex properties.
+class TapDetector extends StatelessWidget {
+  /// What gets done after a tap event.
+  final Function tapCallback;
+
+  /// If there is no Flex amount it will cause errors if under a Column / Row.
+  final int flexAmount;
+
+  /// The container background color (optional). Example:
+  /// ```dart
+  /// Colors.lightGreen.withOpacity(0.3)
+  /// ```
+  /// would give a semi-transparent color so you can position the [TapDetector].
+  final color;
+
+  const TapDetector({@required this.flexAmount, this.tapCallback, this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      flex: flexAmount,
+      child: Container(
+        color: color,
+        child: GestureDetector(
+          behavior: HitTestBehavior.translucent,
+          onTap: tapCallback,
+        ),
+      ),
+    );
   }
 }
